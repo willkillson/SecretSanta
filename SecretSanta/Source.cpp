1ed4b0f3730a4741544cff0a9a56d46f989f0fa9
willkillson <willkillson@gmail.com>
2017-12-15 12:53:37 -0700

    d

--- SecretSanta/Source.cpp
+++ SecretSanta/Source.cpp
@@ -2,13 +2,34 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
+
+
+
+#include <cstdlib> //std::rand, std::srand
+#include <ctime> //std::time
+#include <algorithm> //std::random_shuffle
 
 #define IN_FILEPATH "../lists"
 #define OUT_FILEPATH "../lists"
 
-int main()
-{
 
+class People {
+
+public:
+
+	std::string getFullName() { std::string fname; fname = _firstname + " " + _lastname; return fname; }//this just catenates first and last names
+	std::string getFirstName() { return this->_firstname; }
+	std::string getLastName() { return this->_lastname; }
+	void setFirstName(std::string nametoset) { this->_firstname = nametoset; }
+	void setLastName(std::string nametoset) { this->_lastname = nametoset; }
+private:
+	std::string _firstname;
+	std::string _lastname;
+};
+
+void readInData(std::vector <People> &list)
+{
 	std::ostringstream inputFilePath;
 	std::ostringstream myOutputFilePath;
 
@@ -17,11 +38,13 @@ int main()
 
 	std::ifstream input;
 	input.open(inputFilePath.str());
-	
+
 
 	std::string line;
 	std::string first;
 	std::string last;
+
+
 
 	while (1)
 	{
@@ -34,14 +57,50 @@ int main()
 		getline(input, line);
 		std::istringstream ss(line);
 
+		ss >> first;
+		ss >> last;
 
-		std::cout << line << std::endl;
+		People person;
+		person.setFirstName(first);
+		person.setLastName(last);
 
-
+		list.push_back(person);
 
 	}
 
+}
+void printList(std::vector <People> list)
+{
+
+	std::cout << "List total is ";
+	std::cout << list.size() << std::endl << std::endl;
+
+	for (int i = 0; i < list.size(); i++)
+	{
+		std::cout << list[i].getFullName() << std::endl;
+	}
+
+}
 
 
 
+
+int myrandom(int i) { return std::rand() % i; }
+
+
+
+
+int main()
+{
+
+
+	std::vector <People> rawlist; 
+	readInData(rawlist);//read in the data
+
+	std::vector <People> Alist;
+	std::vector <People> Blist;
+
+
+
+	printList(rawlist);//print out the list
 }
