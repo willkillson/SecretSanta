5c50ff23a0ad76618306de0da8a666fbbfef1a5f
willkillson <willkillson@gmail.com>
2017-12-15 12:26:55 -0700

    d

--- SecretSanta/Source.cpp
+++ SecretSanta/Source.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+
+#define IN_FILEPATH "../lists"
+#define OUT_FILEPATH "../lists"
 
 int main()
 {
-	int poop;
-	std::cout << "Hello world!" << std::endl;
+
+	std::ostringstream inputFilePath;
+	std::ostringstream myOutputFilePath;
+
+	inputFilePath << IN_FILEPATH << "/names.txt";
+	//need to make outputs for every name
+
+	std::ifstream input;
+	input.open(inputFilePath.str());
+	
+
+	std::string line;
+	std::string first;
+	std::string last;
+
+	while (1)
+	{
+
+		if (input.eof() == true)
+		{
+			break;
+		}
+
+		getline(input, line);
+		std::istringstream ss(line);
+
+
+		std::cout << line << std::endl;
+
+
+
+	}
+
+
+
 
 }
